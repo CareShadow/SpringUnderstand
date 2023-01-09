b24ed906972041c49a9bf4b6c9a4bdc15366050e
careshadow <1845161518@qq.com>
2023-01-09 21:02:13 +0800

    MessageConverter的作用, @ResponseBody是返回值处理器解析的,但具体转换工作是MessageConverter实现的, 返回值处理器的列表,及其4种不走视图解析器的返回值处理器

--- src/main/java/com/shadow/day14/Day14Application.java
+++ src/main/java/com/shadow/day14/Day14Application.java
@@ -5,14 +5,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.core.DefaultParameterNameDiscoverer;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.method.annotation.ExpressionValueMethodArgumentResolver;
+import org.springframework.web.method.annotation.ModelFactory;
 import org.springframework.web.method.annotation.RequestHeaderMethodArgumentResolver;
 import org.springframework.web.method.annotation.RequestParamMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolverComposite;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.mvc.method.annotation.*;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 /**
@@ -23,6 +27,15 @@ import java.util.List;
  * @Version 1.0
  **/
 public class Day14Application {
+    // @ModelAttribute 和 @InitBinder 在Controller和ControllerAdvice一样
+
+    /*
+        a. MessageConverter的作用, @ResponseBody是返回值处理器解析的,但具体转换工作是MessageConverter实现的
+        b. MediaType
+            - 首先看@RequestMapping上有没有指定
+            - 其次看request的Accept头有没有指定
+            - 按MessageConverter的顺序决定
+     */
     public static void main(String[] args) throws Exception {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(WebConfig.class);
         ServletInvocableHandlerMethod handlerMethod = new ServletInvocableHandlerMethod(new WebConfig.Controller1(),
@@ -36,9 +49,21 @@ public class Day14Application {
         handlerMethod.setHandlerMethodArgumentResolvers(getArgumentResolvers(context));
         // 准备一个Model模型
         ModelAndViewContainer modelAndViewContainer = new ModelAndViewContainer();
+
+        RequestMappingHandlerAdapter adapter = new RequestMappingHandlerAdapter();
+        adapter.setApplicationContext(context);
+        adapter.afterPropertiesSet();
         // 准备一个MockHttp请求
         MockHttpServletRequest request = new MockHttpServletRequest();
         request.setParameter("name", "张三");
+
+        // 准备一个模型工厂方法 用来解析@ModelAttribute用作方法上的, 参数上由参数解析器ServletModelAttributeMethodProcessor来进行解析
+        Method getModelFactory = RequestMappingHandlerAdapter.class.getDeclaredMethod("getModelFactory", HandlerMethod.class, WebDataBinderFactory.class);
+        getModelFactory.setAccessible(true);
+        ModelFactory modelFactory = (ModelFactory) getModelFactory.invoke(adapter, handlerMethod, dataBinderFactory);
+        modelFactory.initModel(new ServletWebRequest(request), modelAndViewContainer, handlerMethod);
+
+        // handlerMethod因该是一个调用控制器的类
         handlerMethod.invokeAndHandle(new ServletWebRequest(request), modelAndViewContainer);
 
         System.out.println(modelAndViewContainer.getModel());
--- src/main/java/com/shadow/day14/WebConfig.java
+++ src/main/java/com/shadow/day14/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,8 +19,20 @@ import org.springframework.web.servlet.ModelAndView;
 @Configuration
 @ComponentScan
 public class WebConfig {
+    @ControllerAdvice
+    static class MyControllerAdvice {
+        @ModelAttribute("a")
+        public String aa() {
+            return "aa";
+        }
+    }
+
     @Controller
     static class Controller1 {
+        @ModelAttribute("b")
+        public String aa() {
+            return "bb";
+        }
         // 用于解决返回值解析器的缺失
         @ResponseStatus(HttpStatus.OK)
         public ModelAndView foo(@ModelAttribute("u") User user) {
