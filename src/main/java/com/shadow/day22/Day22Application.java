51172279a054567d34c5953b56db273c4e126de4
careshadow <1845161518@qq.com>
2023-03-02 20:43:06 +0800

    Bean的几种实例化方式 ServiceLoader ServiceLoaderBeanFactory AutoWireCapableBeanFactory#createBean(Class<?>)

--- src/main/java/com/shadow/day22/Day22Application.java
+++ src/main/java/com/shadow/day22/Day22Application.java
@@ -1,7 +1,12 @@
 package com.shadow.day22;
 
-import com.shadow.day21.User;
+import com.shadow.day22.factory.DefaultUserFactory;
+import com.shadow.day22.factory.UserFactory;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Iterator;
+import java.util.ServiceLoader;
 
 /**
  * @ClassName Day22Application
@@ -12,9 +17,29 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  **/
 public class Day22Application {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/bean03.xml");
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("META-INF/bean03.xml");
+        AutowireCapableBeanFactory beanFactory = applicationContext.getAutowireCapableBeanFactory();
         // User user = context.getBean("userTest", User.class);
-        User user = context.getBean("userFactoryBean", User.class);
-        System.out.println(user);
+        // User user = context.getBean("userFactoryBean", User.class);
+        // System.out.println(user);
+        // demoServiceLoader();
+        ServiceLoader loader = applicationContext.getBean("serviceLoaderFactoryBean", ServiceLoader.class);
+        // demoServiceLoaderBeanFactory(loader);
+        DefaultUserFactory bean = beanFactory.createBean(DefaultUserFactory.class);
+        System.out.println(bean.createUser());
+    }
+
+    // ServiceLoader since 1.6 两者区别在于ServiceLoaderBeanFactory有多种实现会逐行输出
+    public static void demoServiceLoader() {
+        ServiceLoader<UserFactory> load = ServiceLoader.load(UserFactory.class, Thread.currentThread().getContextClassLoader());
+        demoServiceLoaderBeanFactory(load);
+    }
+
+    public static void demoServiceLoaderBeanFactory(ServiceLoader serviceLoader) {
+        Iterator<UserFactory> iterator = serviceLoader.iterator();
+        while (iterator.hasNext()) {
+            UserFactory next = iterator.next();
+            System.out.println(next.createUser());
+        }
     }
 }
